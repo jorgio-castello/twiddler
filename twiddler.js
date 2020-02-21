a8e9b851cb58d5624c2d388af28e86b43260effe
Jorge Castello <jorgio.castello@gmail.com>
2020-02-20 21:11:12 -0800

    Create generateTimeStamp function
    generateTimeStamp accepts a JavaScript date object
    generateTimeStamp returns a formatted date to include in the tweet
    
    Steps:
      0. Declare an array called months, that includes the names of months
      1. Declare day, month, and year
        1a. These are assigned the values present in JS date object
        1b. Example: day = date.getDate();
      2. Declare hours, minutes, and amPM according to JS date object
        2a. Note: hours / amPM will be reformatted according to normal
        time standards
        2b. if hours is greater than 12, subtract 12 from hours, ampPM = 'PM'
            if hours === 0, add 12 to hours
            Note: Default value for amPM is 'AM'
      3. Declare and return formatted version of timeStamp at tweet
        3a. Example: Twiddled at 8:07PM on February 20, 2020

--- twiddler.js
+++ twiddler.js
@@ -1,3 +1,27 @@
+//generateTimeStamp accepts a date
+//generateTimeStamp return a string to include in a new tweet
+function generateTimeStamp(date) {
+  //0. List of full month names to use in a new tweet
+  let months = ['January', 'February', 'March', 'April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  //1. Declare current day, month, & year based on date parameter
+  let day = date.getDate(), year = date.getFullYear(), month = months[date.getMonth()];
+
+  //2a. Declare hours / minutes / amPM of tweet
+  //2b. hours / amPM will be re-assigned based on whether it is AM or PM at time of tweet
+  let hours = date.getHours(), minutes = date.getMinutes(), amPM = 'AM';
+  minutes = String(minutes).padStart(2, '0'); //if minutes is less than 10, add a 0 in front
+
+  //2c.Re-assign hours / amPM based on the following:
+  //if hours = 0, hours should equal 12, and amPM should equal AM
+  //else if hours > 12, hours should equal hours less 12, and time should be PM
+  if(hours > 12) hours -= 12, amPM = 'PM';
+  if(hours === 0) hours = 12, amPM = 'AM';
+
+  //3. Declare and return formatted timeStamp – ex: Twiddled at 8:07PM on February 20, 2020
+  let timeStamp = ` Twiddled at ${hours}:${minutes} ${amPM} on ${month} ${day}, ${year}`
+  return timeStamp;
+}
 
 //generateTweet accepts a tweet from streams.home
 //generateTweet pushes the tweet to the DOM
@@ -6,7 +30,7 @@ function generateTweet(tweet) {
   //Declare the inputs of a tweet: user, message, and timeStamp
   let user = `@${tweet.user}`;
   let message = `: ${tweet.message}`;
-  let timeStamp = new Date();
+  let timeStamp = generateTimeStamp(new Date());
 
   let $tweet = $('<div></div>');
   $tweet.text(user + message + timeStamp);
